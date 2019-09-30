bbfdd676503c35b9e3e98bd24ad6d374ef7af3d2
Roman Saushkin <saushkin.roman@mail.ru>
2019-09-30 08:18:37 +0300

    Added DbSetup script

---
+++ DbSetup.sql
@@ -0,0 +1,46 @@
+﻿CREATE DATABASE SiteMonitor
+GO
+
+USE SiteMonitor
+
+CREATE TABLE dbo.SiteConfiguration 
+(
+	Id INT NOT NULL IDENTITY PRIMARY KEY,
+	SiteUrl VARCHAR(MAX) NOT NULL,
+	SiteStatusCheckIntervalTypeId INT NOT NULL,
+	SiteStatusCheckInterval INT NOT NULL,
+	LastUpdated DATETIME NOT NULL
+)
+
+CREATE TABLE dbo.SiteStatusCheckIntervalType
+(
+	Id INT NOT NULL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL
+)
+
+INSERT INTO dbo.SiteStatusCheckIntervalType
+(
+	Id,
+	Name
+)
+VALUES
+	(1, 'Seconds'),
+	(2, 'Minutes'),
+	(3, 'Hours')
+
+
+ALTER TABLE dbo.SiteConfiguration
+   ADD CONSTRAINT FK_SiteConfiguration_SiteStatusCheckIntervalTypeId FOREIGN KEY (SiteStatusCheckIntervalTypeId)
+      REFERENCES dbo.SiteStatusCheckIntervalType (Id)
+
+INSERT INTO dbo.SiteConfiguration
+(
+	SiteUrl,
+	SiteStatusCheckIntervalTypeId,
+	SiteStatusCheckInterval,
+	LastUpdated
+)
+VALUES
+	('https://www.google.com/', 1, 30, GETUTCDATE()),
+	('https://mail.ru/', 2, 1, GETUTCDATE()),
+	('https://fakesitename.com/', 2, 3, GETUTCDATE())
